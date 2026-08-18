da96dfc01d5b03cd60cbb88d634d58cfc050faf8
Touseef231 <touseefjutt6565@gmail.com>
2026-08-18 14:02:00 +0500

    Auto-update: 2026-08-18 14:01:59

---
+++ First 21 days journey/Day 1 project.py
@@ -0,0 +1,8 @@
+# This project i day 1 project and uses simple things like print and input nothing else 
+# projects will be more stronger a dy increases
+print("Enter a noun, verb and adjective(put spaces between them and enter in one line)")
+noun, verb ,adjective = input("").split()
+print(f"""One day, a very {adjective} {noun} decided to {verb} through the city.
+Everyone stopped and stared because nobody had ever seen anything like it before.
+The {noun} then shouted, “I am the greatest {noun} alive!”
+And somehow, the whole city started celebrating.""")
