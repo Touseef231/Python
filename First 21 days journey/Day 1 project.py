abecf89dc4b86685dacbd4185bd1a8e0250b2786
Touseef231 <touseefjutt6565@gmail.com>
2026-08-18 14:03:39 +0500

    Auto-update: 2026-08-18 14:03:38

--- First 21 days journey/Day 1 project.py
+++ First 21 days journey/Day 1 project.py
@@ -1,5 +1,6 @@
 # This project i day 1 project and uses simple things like print and input nothing else 
 # projects will be more stronger a dy increases
+# In this project you enter three things Noun, Verb and Adjective and it gives you story with things you entered.:3
 print("Enter a noun, verb and adjective(put spaces between them and enter in one line)")
 noun, verb ,adjective = input("").split()
 print(f"""One day, a very {adjective} {noun} decided to {verb} through the city.
